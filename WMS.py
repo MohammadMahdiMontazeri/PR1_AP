7fa8db7129e42c596b3546ecf87c9b1e9a529c60
Mohammad <montazeriyazd1@gmail.com>
2023-04-22 12:49:48 +0330

    WMS : making folder , cleaning the code

--- WMS.py
+++ WMS.py
@@ -3,27 +3,27 @@ import os
 
 class WarehouseManagmetSystem:
     def __init__(self):
-        self.warehouse1 = pd.read_csv('/Users/mohammad/Desktop/PR1_AP/data/Warehouse1.csv')
+        self.warehouse1 = pd.read_csv('/Users/mohammad/Desktop/PR1_AP/warehouse_data/Warehouse1.csv')
 
     def new_warehouse(self):
         n = 1
-        for file in os.listdir('/Users/mohammad/Desktop/PR1_AP/data/'):
+        for file in os.listdir('/Users/mohammad/Desktop/PR1_AP/warehouse_data/'):
             if file.endswith('.csv'):
                 n += 1
         warehouse_name = f'warehouse{n}'
-        df = pd.DataFrame({'id':[],'name': [],'stock': []})
-        df.to_csv(f'/Users/mohammad/Desktop/PR1_AP/data/{warehouse_name}.csv' , index = False)
+        df = pd.DataFrame({'id':[],'stock': []})
+        df.to_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv' , index = False)
 
-        setattr(WarehouseManagmetSystem, warehouse_name, pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/data/{warehouse_name}.csv'))
+        setattr(WarehouseManagmetSystem, warehouse_name, pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv'))
 
-    def add_product(self, warehouse_number , no : int , name :str , stock : int):
+    def add_product(self, warehouse_number , no : int , stock : int):
         warehouse_name = f'warehouse{warehouse_number}'
-        df = pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/data/{warehouse_name}.csv')
-        dfn = pd.DataFrame({'id':[no],'name': [name],'stock': [stock]})
+        df = pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv')
+        dfn = pd.DataFrame({'id':[no],'stock': [stock]})
         df = pd.concat([dfn , df])
-        df.to_csv(f'/Users/mohammad/Desktop/PR1_AP/data/{warehouse_name}.csv' , index = False)
+        df.to_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv' , index = False)
         
-        setattr(WarehouseManagmetSystem, warehouse_name, pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/data/{warehouse_name}.csv'))
+        setattr(WarehouseManagmetSystem, warehouse_name, pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv'))
 
     def update_warehouse(self, warehouse_number , file_location):
         f = file_location.split('.')
@@ -35,7 +35,13 @@ class WarehouseManagmetSystem:
             df = pd.read_csv(file_location)
         
         warehouse_name = f'warehouse{warehouse_number}'     
-        ware_old = pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/data/{warehouse_name}.csv')
+        ware_old = pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv')
+        #
+        
+        ware_new.to_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv' , index = False)
+        
+        setattr(WarehouseManagmetSystem, warehouse_name, pd.read_csv(f'/Users/mohammad/Desktop/PR1_AP/warehouse_data/{warehouse_name}.csv'))
+
         
 
 
