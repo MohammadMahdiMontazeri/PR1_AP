6c431942001ebb6d836107092112f672b8f21988
Amyar Siosemardeh <siosemardehamyar@gmail.com>
2023-05-02 01:50:03 +0430

    STORE : made changes and bug fixes

--- Store.py
+++ Store.py
@@ -22,8 +22,9 @@ store.warehouse.new_warehouse()
 
 
 def start():
-    print('1.Seller\n2.Customer')
+    print('1.Seller\n2.Customer\n')
     ne = input()
+    print('__________________________________________________________________')
     if ne == '1':
         return seller()
     elif ne == '2':
@@ -31,27 +32,35 @@ def start():
         return customer()
     
 def seller():
-    print('1.Create a new warehouse\n2.Add product\n3.Update the warehouse\n4.Warehouse status\n5.The state of the accounting system\n# start')
+    print('1.Create a new warehouse\n2.Add product\n3.Update the warehouse\n4.Warehouse status\n5.The state of the accounting system\ns.start')
     ne = input()
+
 
     if ne == '1':
         store.warehouse.new_warehouse()
+        print('__________________________________________________________________')
         print('A new warehouse created.\n')
+        print('__________________________________________________________________')
         return seller()
     
     elif ne == '2':
+        print('__________________________________________________________________')
         return add_product()
         
     elif ne == '3':
+        print('__________________________________________________________________')
         return choose1()
     
     elif ne == '4':
+        print('__________________________________________________________________')
         return choose2()
 
     elif ne == '5':
+        print('__________________________________________________________________')
         pass
     
-    elif ne == '#':
+    elif ne == 's':
+        print('__________________________________________________________________')
         return start()
 
 def add_product():
@@ -62,13 +71,18 @@ def add_product():
         price = int(input('Please enter the price of the product.\n'))
         store.warehouse.add_product(ware_number , no , stock , price)
         print('Product added successfully.\n')
+        print('__________________________________________________________________')
+
         return seller()
     else:
         print('warehouse number is incorrect.\n')
+        print('__________________________________________________________________')
+
         return add_product()
     
 def choose1():
-    nx = input('1.Update with file location\n2.Update manually\n* back\n# start\n')  
+    nx = input('1.Update with file location\n2.Update manually\nb.back\ns.start\n')  
+    print('__________________________________________________________________')
 
     if nx == '1':
         return file_locations()
@@ -76,10 +90,10 @@ def choose1():
     if nx == '2':
         return update_manuall()
     
-    elif nx == '*':
+    elif nx == 'b':
         return seller()
     
-    elif nx == '#':
+    elif nx == 's':
         return start()
     
 def file_locations():
@@ -88,9 +102,13 @@ def file_locations():
         file_location = input('Please enter the file location\n')
         store.warehouse.update_warehouse(ware_number , file_location)
         print(f'Warehouse number {ware_number} updated.\n')
+        print('__________________________________________________________________')
+
         return choose1()
     else:
         print('warehouse number is incorrect.\n')
+        print('__________________________________________________________________')
+
         return file_locations()
 
 def update_manuall():
@@ -100,13 +118,17 @@ def update_manuall():
     if store.warehouse.check_warehouse_number(warehouse_number):
         store.warehouse.update_warehouse_manual(warehouse_number , no , stock)
         print(f'Warehouse number {warehouse_number} updated.\n')
+        print('__________________________________________________________________')
+
         return choose1()
     else:
         print('warehouse number is incorrect.\n')
+        print('__________________________________________________________________')
+
         return update_manuall() 
     
 def choose2():
-    nx = input('1.csv\n2.txt\n* back\n# start\n')  
+    nx = input('1.csv\n2.txt\nb.back\ns.start\n')  
 
     if nx == '1':
         return csv()
@@ -114,55 +136,100 @@ def choose2():
     elif nx == '2':
         return txt()
     
-    elif nx == '*':
+    elif nx == 'b':
+        print('__________________________________________________________________')
         return seller()
     
-    elif nx == '#':
+    elif nx == 's':
+        print('__________________________________________________________________')
         return start()
 
 def csv():
-    store.warehouse.warehouse_status('csv')
+    print('__________________________________________________________________')
+    file_locationl = input('Please enter the file location\n')
+    print('__________________________________________________________________')
+
+    store.warehouse.warehouse_status('csv' , file_locationl)
     print('The warehouse status was saved as csv\n')
+    print('__________________________________________________________________')
+
     return choose2()
 
 def txt():
-    store.warehouse.warehouse_status('txt')
+    print('__________________________________________________________________')
+    file_locationl = input('Please enter the file location\n')
+    print('__________________________________________________________________')
+
+    store.warehouse.warehouse_status('txt' , file_locationl)
     print('The warehouse status was saved as txt\n')
+    print('__________________________________________________________________')
+
     return choose2()
 
 def customer():
     df = pd.read_csv(f'{cwd}/total_stock/total_stock.csv')
     df.loc[df['stock'] == 0 , 'price'] = 'unavailable'
-    print(df,'\n\nPleaes enter a ID for adding to your cart\n&.cart\n$.settlement\n# start\n')
+    print(df,'\n\nPleaes enter a ID for adding to your cart\nc.cart\nf.settlement\ns.start\n')
     nc = input()
+    print('__________________________________________________________________')
 
-    if nc == '&':
+    if nc == 'c':
         return cart()
     
-    elif nc == '$':
+    elif nc == 'f':
         return settlement()
     
-    elif nc == '#':
+    elif nc == 's':
         return start()
     
     else:
         if store.warehouse.check_ID_is_correct(nc):
             if store.warehouse.total_stock()[nc] == 0 :
-                print('Sorry, thid product is currently unavailable\n')
+                print('Sorry, thid product is currently unavailable.\n')
+                print('__________________________________________________________________')
+
                 return customer()
             else:
-                stock = int(input('Pleas ......\n'))
+                stock = int(input('How many unints do you want:\n'))
                 if store.warehouse.total_stock()[nc] >= stock:
-                    print ('mitone bekhare')
+                    store.order_registration.add_to_cart(nc , str(stock))
+                    print('Your cart has been updated.\n')
+                    print('__________________________________________________________________')
+
                     return customer()
                 else:
-                    print('nemitone')
+                    print('Sorry, there are not enough units in the store.\n')
+                    print('__________________________________________________________________')
+
+                    return customer()
         else:
             print('The ID you entered is incorrect, please try again.\n')
+            print('__________________________________________________________________')
+
             return customer()
 
+def cart():
+    if len(store.order_registration.cart.keys()) != 0:
+        store.order_registration.add_price_to_cart()
+        store.order_registration.show_cart()
+        print('b.back\nf.settlement\n')
+        nw = input()
+        if nw == 'b':
+            print('__________________________________________________________________')
+            return customer()
+        
+        elif nw == 'f':
+            print('__________________________________________________________________')
+            return final()
+        
+    else:
+        print('Your cart is empty.\n')
+        print('__________________________________________________________________')
+
+        return customer()
 
 
+print('\n')
 for i in trange(20, file=sys.stdout, desc='starting'):
     time.sleep(.3)
 print('\n')
