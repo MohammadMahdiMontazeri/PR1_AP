2a0b09faaf1ac595220fb7472eef7b13a78fa701
Amyar Siosemardeh <siosemardehamyar@gmail.com>
2023-05-01 23:36:34 +0430

    AMS & ORS : defined new functions

--- ORS.py
+++ ORS.py
@@ -3,16 +3,19 @@ import random
 import os
 import time
 cwd = os.getcwd()
-cart = {}
 
 class OrderRegistrationSystem:
+    def __init__(self):
+        self.cart = {}
+
     def add_to_cart(self,no,amount):
-        cart.update({no : amount})
+        self.cart.update({no : amount})
+
 
     def remove_from_cart(self,no):
-        if no in cart.keys():
-            if cart[no] >= 1:
-                cart[no] -= 1
+        if no in self.cart.keys():
+            if int(self.cart[no]) >= 1:
+                self.cart[no] = str(int(self.cart[no]) - 1)
 
     def add_price_to_cart(self): 
         df = pd.read_csv(f'{cwd}/total_stock/total_stock.csv')
@@ -22,7 +25,7 @@ class OrderRegistrationSystem:
         for i in df.loc[:, 'id']:
             for j in df.loc[:, 'price']:
                 cart_with_price.update({i : j})
-        cart2 = {k : [cart.get(k),cart_with_price.get(k)] for k in cart.keys() | cart_with_price.keys()}
+        cart2 = {k : [self.cart.get(k),cart_with_price.get(k)] for k in self.cart.keys() | cart_with_price.keys()}
         cart2 = {k: v for k, v in cart2.items() if v[0] != None}
         for c in cart2:
             cart2[c] = tuple(cart2[c])
@@ -39,13 +42,24 @@ class OrderRegistrationSystem:
         df['Total unit price'] = df ['Price'] * df['Stock']
         total_price = df.loc[:, 'Total unit price'].sum()
         print(f'{df}\n\nYour total price is : {total_price}\n')
-        
-        
-    def confirmation_and_factor(self,card_number , full_name, addres  ,phone_number ,timee , delivery_type):
+    
+    def empty_cart(self):
+        self.cart = {}
+        df = pd.DataFrame()
+        df.to_csv(f'{cwd}/cart/cart.csv')
+
+    def generate_random_number(self):
+        return random.randint(10**10, 10**11 - 1)
+
+    def sum_of_stock(self):
+        if confirmation_and_factor :
+            sum_stock = sum(int(self.cart.values))
+            return sum_stock
+
+    def confirmation_and_factor(self,card_number , full_name, addres  ,phone_number ,timee , delivery_type, rnd):
         n = 0
         m = 0
         if len(card_number) == 16 and card_number.isdigit():
-            rand = random.randint(10**10, 10**11 - 1)
             for file in os.listdir(f'{cwd}/ors/confirmation/successful'):
                 if file.endswith('.txt'):
                     n += 1
@@ -60,8 +74,9 @@ class OrderRegistrationSystem:
             factor_path = f'{cwd}/ors/factor/factor{m}.txt'
             cart = pd.read_csv(f'{cwd}/cart/cart.csv')[['ID','Price']]
             file = open(factor_path,'w')
-            file.write(f'\n{cart}\n\nYour addres is : {addres}.\nName : {full_name}.\nDelivery time :{timee}.\nYour delivery type is : {delivery_type}')
+            file.write(f'\n{self.cart}\n\nYour addres is : {addres}.\nOrder number : {rnd}.\nName : {full_name}.\nDelivery time :{timee}.\nYour delivery type is : {delivery_type}')
             file.close()
+            return True
 
         else:
             for file in os.listdir(f'{cwd}/ors/confirmation/unsuccessful'):
@@ -71,3 +86,17 @@ class OrderRegistrationSystem:
             file = open(file_path1,'w')
             file.write(f'Card number : {card_number}.The order registration was not successful.Name : {full_name}')
             file.close()
+    
+    def total_price(self):
+        df = pd.read_csv(f'{cwd}/cart/cart.csv')
+        df['Total unit price'] = df ['Price'] * df['Stock']
+        total_price = df.loc[:, 'Total unit price'].sum()
+
+    def accounting(self , Stock, Order_No, Total_price, Transport_Price):
+        if confirmation_and_factor == True :
+            df = pd.read_csv(f'{cwd}/ams/Accounting.csv')
+            dfn = pd.DataFrame({'Stock':[Stock] , 'Order No':[Order_No], 'Total price':[Total_price], 'Transport Price':[Transport_Price]})
+            dfn['Tax'] = (dfn['Total price'] + dfn   ['Transport Price']) * 1.09
+            df = pd.concat([df,dfn])
+            df.to_csv(f'{cwd}/ams/Accounting.csv')
+
